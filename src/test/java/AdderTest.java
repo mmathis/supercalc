1bd69c9432ad31554ceee249a9f137c30f3a95b4
mmathis <mmathis@3e78270b-776e-4cbb-b617-6cbe50154442>
2012-01-27 23:04:11 +0000

    Implemented test for adder
    git-svn-id: http://192.168.1.23/svn/supercalc/trunk@6 3e78270b-776e-4cbb-b617-6cbe50154442

---
+++ src/test/java/AdderTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import supercalc.Adder;
+
+
+public class AdderTest {
+
+	Adder adder = new Adder();;
+	
+	@Test
+	public void add() {
+
+		assertEquals(Integer.valueOf(7), adder.add(5, 2));
+	}
+}
