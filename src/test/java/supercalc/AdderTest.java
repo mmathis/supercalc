fb0c73a71132b8a47e7095e0997ba505923a579c
Michel Mathis <mathis@mptechnology.ch>
2012-01-28 13:23:42 +0100

    added test with negative numbers

--- src/test/java/supercalc/AdderTest.java
+++ src/test/java/supercalc/AdderTest.java
@@ -15,4 +15,10 @@ public class AdderTest {
 
 		assertEquals(Integer.valueOf(7), adder.add(5, 2));
 	}
+	
+	@Test
+	public void negative() {
+
+		assertEquals(Integer.valueOf(-3), adder.add(-5, 2));
+	}
 }
